2271a1fece3f82244d1104404e5980b32af4148f
nowseen <nowseen.bd@asa-international.com>
2022-11-17 16:08:46 +0600

    add auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,85 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Card } from './models/card.model';
+import { CardsService } from './service/cards.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'first-angular-project';
+  cards: Card[] =[];
+  card: Card ={
+    id: '',
+    villainName: '',
+    franchise:'',
+    powers:'',
+    imageURL: ''
+  }
+
+  constructor(private cardService:CardsService){
+   
+  }
+  ngOnInit(): void {
+    this.getAllCards();
+   
+  }
+  getAllCards(){
+    this.cardService.getAllCards()
+    .subscribe(
+      response =>{
+        this.cards=response;
+      
+        // console.log(response);
+      }
+    )
+  }
+  OnSubmit(){
+    if(this.card.id ===''){
+      this.cardService.Post(this.card)
+      .subscribe(
+        response=>{
+          this.getAllCards();
+          this.card= {
+            id: '',
+            villainName: '',
+            franchise:'',
+            powers:'',
+            imageURL: ''
+          };
+        }
+      );
+    }
+    else {
+      this.updateCard(this.card);
+    }
+
+    
+//  console.log(this.card);
+  }
+  deleteCard(id: string){
+    this.cardService.deleteCard(id)
+    .subscribe(
+      response=>{
+        this.getAllCards();
+      }
+    )
+
+  }
+  populateForm(card:Card){
+    this.card = card;
+
+  }
+  updateCard(card: Card){
+    console.log(card)
+    this.cardService.updateCard(card)
+    
+    .subscribe(
+      response =>{
+        // console.log(response)
+        this.getAllCards();
+      }
+    )
+  }
 }
