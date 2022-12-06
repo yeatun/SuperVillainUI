1a9396dcf088b1a093f77abd2818073b5b99e19f
yeatun <p.yeatun@gmail.com>
2022-12-06 16:34:53 +0600

    update path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 
 const routes: Routes = [
+  {path:'',component: LoginComponent},
   {path:'login',component: LoginComponent},
   {path:'signup',component: SignupComponent},
   {path:'dashboard',component: DashboardComponent}
